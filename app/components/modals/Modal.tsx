a8e71c791949107452573b63784e219b565dde3a
Patryk Polanski <pat.polanski@gmail.com>
2024-02-03 00:29:12 +0000

    Register modal: add footer content (google and github signup placeholders)

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -100,6 +100,7 @@ export default function Modal({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,6 +11,7 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../Inputs/Input';
+import Button from '../Button';
 
 export default function RegisterModal() {
   const registerModal = useRegisterModal();
@@ -76,6 +77,35 @@ export default function RegisterModal() {
     </div>
   );
 
+  const footerContent = (
+    <div className='flex flex-col gap-4 mt-3'>
+      <hr />
+      <Button
+        outline
+        label='Continue with Google'
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label='Continue with Github'
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className='text-neutral-500 text-center mt-4 font-light'>
+        <div className='justify-center flex flex-row items-center gap-2'>
+          <div>Already have an account?</div>
+          <div
+            onClick={registerModal.onClose}
+            className='text-neutral-800 cursor-pointer hover:underline'
+          >
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <Modal
       disabled={isLoading}
@@ -85,6 +115,7 @@ export default function RegisterModal() {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 }

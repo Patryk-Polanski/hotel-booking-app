c6aa0698a402dd0d222b12512506282187d4de8d
Patryk Polanski <pat.polanski@gmail.com>
2024-02-09 01:25:17 +0000

    create heartButton, style ListingCard

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -2,11 +2,15 @@
 
 import { useCallback, useMemo } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 import { Listing, Reservation } from '@prisma/client';
 import { format } from 'date-fns';
 
 import { SafeUser } from '@/app/types';
 import useCountries from '@/app/hooks/useCountries';
+
+import HeartButton from '../HeartButton';
+import Button from '../Button';
 
 interface ListingCardProps {
   data: Listing;
@@ -59,5 +63,42 @@ export default function ListingCard({
     return `${format(start, 'PP')} - ${format(end, 'PP')}`;
   }, [reservation]);
 
-  return <div>ListingCard</div>;
+  return (
+    <div
+      onClick={() => router.push(`/listings/${data.id}`)}
+      className='col-span-1 cursor-pointer group'
+    >
+      <div className='flex flex-col gap-2 w-full'>
+        <div className='aspect-square w-full relative overflow-hidden rounded-xl'>
+          <Image
+            fill
+            alt={`listing - ${data.title}`}
+            src={data.imageSrc}
+            className='object-cover h-full w-full group-hover:scale-110 transition'
+          />
+          <div className='absolute top-3 right-3'>
+            <HeartButton listingId={data.id} currentUser={currentUser} />
+          </div>
+        </div>
+        <div className='font-semibold text-lg'>
+          {location?.region}, {location?.label}
+        </div>
+        <div className='font-light text-neutral-500'>
+          {reservationDate || data.category}
+        </div>
+        <div className='flex flex-row items-center gap-1'>
+          <div className='font-semibold'>$ {price}</div>
+          {!reservation && <div className='font-light'>night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
+      </div>
+    </div>
+  );
 }

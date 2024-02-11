9d95f0df818f11bbad7cf81fba10f734c9efba1f
Patryk Polanski <pat.polanski@gmail.com>
2024-02-11 18:00:19 +0000

    create listing head component

--- app/listings/[...listingId]/ListingClient.tsx
+++ app/listings/[...listingId]/ListingClient.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { useMemo } from 'react';
 import { Reservation } from '@prisma/client';
+
 import { SafeListing, SafeUser } from '@/app/types';
+import { categories } from '@/app/components/navbar/Categories';
+
+import Container from '@/app/components/Container';
+import ListingHead from '@/app/components/listings/ListingHead';
 
 interface ListingClientProps {
   reservations?: Reservation[];
@@ -15,5 +21,23 @@ export default function ListingClient({
   listing,
   currentUser,
 }: ListingClientProps) {
-  return <div>ListingClient</div>;
+  const category = useMemo(() => {
+    return categories.find((item) => item.label === listing.category);
+  }, [listing.category]);
+
+  return (
+    <Container>
+      <div className='max-w-screen-lg mx-auto'>
+        <div className='flex flex-col gap-6'>
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+        </div>
+      </div>
+    </Container>
+  );
 }

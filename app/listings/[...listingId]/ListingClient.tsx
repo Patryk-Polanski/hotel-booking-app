58ed591c4c71a9664eb903572e13fa4b287e7ada
Patryk Polanski <pat.polanski@gmail.com>
2024-02-11 18:36:29 +0000

    create ListingInfo and ListingCategory

--- app/listings/[...listingId]/ListingClient.tsx
+++ app/listings/[...listingId]/ListingClient.tsx
@@ -8,6 +8,7 @@ import { categories } from '@/app/components/navbar/Categories';
 
 import Container from '@/app/components/Container';
 import ListingHead from '@/app/components/listings/ListingHead';
+import ListingInfo from './ListingInfo';
 
 interface ListingClientProps {
   reservations?: Reservation[];
@@ -36,6 +37,17 @@ export default function ListingClient({
             id={listing.id}
             currentUser={currentUser}
           />
+          <div className='grid grid-cols-1 md:grid-cols-1 md:gap-10 mt-6'>
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>

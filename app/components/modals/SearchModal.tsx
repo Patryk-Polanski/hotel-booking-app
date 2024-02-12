b9f5aff52d3e535e7370782630180a6d0c10e840
Patryk Polanski <pat.polanski@gmail.com>
2024-02-12 22:45:06 +0000

    searchModal: add body content

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -10,7 +10,10 @@ import { formatISO } from 'date-fns';
 import useSearchModal from '@/app/hooks/useSearchModal';
 
 import Modal from './Modal';
-import { CountrySelectValue } from '../Inputs/CountrySelect';
+import Heading from '../Heading';
+import CountrySelect, { CountrySelectValue } from '../Inputs/CountrySelect';
+import Calendar from '../Inputs/Calendar';
+import Counter from '../Inputs/Counter';
 
 enum STEPS {
   LOCATION = 0,
@@ -110,13 +113,72 @@ export default function SearchModal() {
     return 'Back';
   }, [step]);
 
+  let bodyContent = (
+    <div className='flex flex-col gap-8'>
+      <Heading
+        title='Where do you want to go?'
+        subtitle='Find the perfect location'
+      />
+      <CountrySelect
+        value={location}
+        onChange={(value) => setLocation(value as CountrySelectValue)}
+      />
+      <hr />
+      <Map center={location?.latlng} />
+    </div>
+  );
+
+  if (step === STEPS.DATE) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='When do you plan to go?'
+          subtitle='Make sure everyone is free!'
+        />
+        <Calendar
+          value={dateRange}
+          onChange={(value) => setDateRange(value.selection)}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading title='More information' subtitle='Find your perfect place' />
+        <Counter
+          title='Guests'
+          subtitle='How many guests are coming?'
+          value={guestCount}
+          onChange={(value) => setGuestCount(value)}
+        />
+        <Counter
+          title='Rooms'
+          subtitle='How many rooms do you need?'
+          value={roomCount}
+          onChange={(value) => setRoomCount(value)}
+        />
+        <Counter
+          title='Bathrooms'
+          subtitle='How many bathrooms do you need?'
+          value={bathroomCount}
+          onChange={(value) => setBathroomCount(value)}
+        />
+      </div>
+    );
+  }
+
   return (
     <Modal
       isOpen={searchModal.isOpen}
       onClose={searchModal.onClose}
-      onSubmit={searchModal.onOpen}
+      onSubmit={onSubmit}
       title='Filters'
-      actionLabel='Search'
+      actionLabel={actionLabel}
+      secondaryAction={step === STEPS.LOCATION ? undefined : onBack}
+      secondaryActionLabel={secondaryActionLabel}
+      body={bodyContent}
     />
   );
 }

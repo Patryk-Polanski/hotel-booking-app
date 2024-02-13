0f867f33d45873fd9d07e01f6502e7f17404d2de
Patryk Polanski <pat.polanski@gmail.com>
2024-02-13 13:41:11 +0000

    add error.tsx file, test error handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import getCurrentUser from './actions/getCurrentUser';
-import getListings, { IListingsParams } from './actions/getListings';
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings, { IListingsParams } from "./actions/getListings";
 
-import Container from './components/Container';
-import EmptyState from './components/EmptyState';
-import ListingCard from './components/listings/ListingCard';
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
 interface HomeProps {
   searchParams: IListingsParams;
@@ -19,7 +19,7 @@ export default async function Home({ searchParams }: HomeProps) {
 
   return (
     <Container>
-      <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {listings.map((listing) => {
           return (
             <ListingCard
